b1f790a45827d60ff195d407d945854682e11c5c
Zahuan12 <huanhanquoc1234@gmail.com>
2025-12-30 13:48:18 +0700

    Fix Input Component

--- src/components/molecules/AdminSearchBar/AdminSearchBar.jsx
+++ src/components/molecules/AdminSearchBar/AdminSearchBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Search } from 'lucide-react';
+import Input from '../../atoms/Input/Input';
 
 const AdminSearchBar = ({ 
   value, 
@@ -10,7 +11,7 @@ const AdminSearchBar = ({
   return (
     <div className={`relative flex-1 max-w-md ${className}`}>
       <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
-      <input
+      <Input
         type="text"
         placeholder={placeholder}
         value={value}
